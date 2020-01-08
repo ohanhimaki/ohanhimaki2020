f857a1f70b2bc8027143ea09efedcb6f75f6f630
Olli Hanhimäki <ohanhimaki@gmail.com>
2020-01-08 18:51:26 +0200

    MTS-banneri piiloon jos ei vielä ladannut

--- src/components/mts-banner.tsx
+++ src/components/mts-banner.tsx
@@ -10,7 +10,6 @@ interface lunch {
 const MtsBanner = () => {
   const [ravintola, setRavintola] = useState("")
   const [lista, setLista] = useState("")
-  let readycss = "bg-gray-800 max-w-xl  m-auto p-8 rounded-lg my-4 "
   useEffect(() => {
     fetch("https://mita-tanaan-syotaisiin.herokuapp.com/api/lunchofday")
       .then(result => result.json())
@@ -26,13 +25,10 @@ const MtsBanner = () => {
     ravintola === null ||
     lista === null
   ) {
-    readycss += "mts-hidden"
-  } else {
-    readycss += "mts-show"
+    return <></>
   }
-
   return (
-    <div className={readycss}>
+    <div className="bg-gray-800 max-w-xl  m-auto p-8 rounded-lg my-4 ">
       <h3>Päivän lounaspaikka</h3>
       <h2>{ravintola}</h2>
       <p dangerouslySetInnerHTML={{ __html: lista }}></p>
