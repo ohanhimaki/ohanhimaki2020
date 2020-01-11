750d5357c2851ec8e2dc1efc5b8e031f9c9772fb
Olli Hanhimäki <ohanhimaki@gmail.com>
2020-01-11 16:23:00 +0200

    Eventtien korttia nätimmäksi ja toiminnallisuuksia lisätty, mm. kommenttien + issueiden linkit

--- src/usergithubEvents/usergithubEvent.tsx
+++ src/usergithubEvents/usergithubEvent.tsx
@@ -15,14 +15,25 @@ class UserEvent extends Component {
       datediff: datediff,
     }
     this.divClickFunction = this.divClickFunction.bind(this)
+    this.getSubtitles = this.getSubtitles.bind(this)
   }
 
   divClickFunction(something?: any) {
     this.setState({
       isHidden: !this.state.isHidden,
     })
-    if (something) {
-      window.open(something.html_url)
+    if (something.pull_request) {
+      console.log(something)
+
+      window.open(something.pull_request.html_url)
+      return
+    }
+    // if (something.comment) {
+    //   window.open(something.comment.html_url)
+    //   return
+    // }
+    if (something.issue && !something.comment) {
+      window.open(something.issue.html_url)
     }
   }
 
@@ -33,10 +44,20 @@ class UserEvent extends Component {
   }
 
   getBranch(ref: string) {
+    const types = {
+      default: "",
+      master: "text-red-800",
+      develop: "text-green-800",
+    }
     if (ref != null) {
       const indexOfLastSlash = ref.lastIndexOf("/")
       const branchName = ref.substring(indexOfLastSlash + 1, ref.length)
-      return " - " + branchName
+      return (
+        <span>
+          {" "}
+          - <span className={types[branchName]}>{branchName}</span>
+        </span>
+      )
     }
   }
   getCommitCount(commits: []) {
@@ -44,12 +65,92 @@ class UserEvent extends Component {
       return " - " + commits.length + " Commits"
     }
   }
+  getIssueName(event: any) {
+    let className = ""
+    if (event.payload.issue != null) {
+      let title = event.payload.issue.title
+      if (event.payload.comment == null) {
+        const types = {
+          default: "",
+          closed: "text-red-800",
+          opened: "text-green-800",
+        }
+        className = types[event.payload.action] || types["default"]
+        title = event.payload.action + ": " + title
+      }
+      return (
+        <span>
+          {" "}
+          - <span className={className}>{title} </span>
+        </span>
+      )
+    }
+  }
+  getMoreInfo(payload: any) {
+    if (payload.comment) {
+      return (
+        <div className="flex bg-gray-900 border-gray-800 border-t-2 p-1 mx-3 hover:bg-black">
+          <a
+            href={payload.comment.html_url}
+            target="__blank"
+            className="flex-row w-full"
+          >
+            <h6>{payload.comment.body}</h6>
+          </a>
+        </div>
+      )
+    }
+  }
+  getEventType(eventType: string) {
+    const types = {
+      default: eventType,
+      PushEvent: "Push",
+      IssueCommentEvent: "Issue commented",
+      IssuesEvent: "Issue",
+      PullRequestEvent: "Pull request",
+    }
+
+    return types[eventType] || types["default"]
+  }
+  getPullRequestAction(event: any) {
+    if (event.payload.pull_request) {
+      console.log(event)
+
+      const types = {
+        default: "",
+        closed: "text-red-800",
+        opened: "text-green-800",
+      }
+      return (
+        <span className={types[event.payload.action] || types["default"]}>
+          {} - {event.payload.action}
+          {event.payload.pull_request.merged ? ", merged" : ""}
+        </span>
+      )
+    }
+  }
+
+  getSubtitles(event: any) {
+    const test = (
+      <div>
+        <h4>
+          {this.getEventType(event.type)}
+          {this.getBranch(event.payload.ref)}
+          {this.getCommitCount(event.payload.commits)}
+          {this.getPullRequestAction(event)}
+          {this.getIssueName(event)}
+        </h4>
+      </div>
+    )
+
+    return test
+  }
 
   render() {
     const {
+      getMoreInfo,
       divClickFunction,
-      getBranch,
-      getCommitCount,
+      getSubtitles,
       state: { datediff, isHidden },
       props: { event },
     } = this
@@ -65,14 +166,10 @@ class UserEvent extends Component {
       <div className="flex flex-col bg-gray-900 border-gray-800 border-t-2 ">
         <div
           className="ml-2  w-full flex flex-col p-3 cursor-pointer hover:bg-black"
-          onClick={() => divClickFunction(event.payload.pull_request)}
+          onClick={() => divClickFunction(event.payload)}
         >
           <h3 className=""> {event.repo.name} </h3>
-          <h4>
-            {event.type}
-            {getBranch(event.payload.ref)}
-            {getCommitCount(event.payload.commits)}
-          </h4>
+          {getSubtitles(event)}
           <h4>
             {datediff.timeamount} {} {datediff.timeunit} {} ago
           </h4>
@@ -81,6 +178,7 @@ class UserEvent extends Component {
           {event.payload.commits?.map((value, index: number) => {
             return this.renderCommit(value, index)
           })}
+          {getMoreInfo(event.payload)}
         </div>
       </div>
     )
