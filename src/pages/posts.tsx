7016a915a7c6ae9464d2910b9d0f22191f22f2ce
Olli Hanhimäki <ohanhimaki@gmail.com>
2021-03-14 14:01:56 +0200

    igpostaus omaan komponenttiin ja pientä css fixiä

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -3,48 +3,16 @@ import React from "react"
 import "../../tailwindcss/tailwind.src.css"
 import Layout from "../shared/layout"
 import SEO from "../shared/seo"
-import {graphql, useStaticQuery, Link} from "gatsby"
+import {graphql, useStaticQuery} from "gatsby"
 import PostPreview, {Post} from "../my-posts/post-preview"
-import Img from "gatsby-image";
-import post from "../my-posts/post";
+import IgPostPreview, {IgPost} from "../igPost/igPostPreview";
 
-interface IgPostNode {
-    timestamp: int,
-    original: string,
-    preview: string,
-    localFile: string,
-    caption: string,
-    
-}
-
-interface IgPost {
-    node: IgPostNode;
-}
 
 interface PostContainer {
     source: number;
     date: Date;
     post?: Post;
     igPost?: IgPost;
-    
-}
-
-function IgPostPreview(props: { post: IgPost }) {
-
-    const date = SecondsToDate(props.post.node.timestamp)
-    const formattedDate = 
-        date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear()
-    return <div
-        className="m-4 w-full md:w-1/3 bg-gray-800 p-5 flex-grow rounded-lg shadow-md"
-    >
-        <div className="">
-            <h4>{formattedDate} </h4>
-            <img
-                src={props.post.node.preview}
-            ></img>;
-            <p> {props.post.node.caption} </p>
-        </div>
-    </div>
     
 }
 
@@ -94,6 +62,7 @@ allInstaNode {
   `)
     const postPreviews = data.allMarkdownRemark.edges
     const igposts = data.allInstaNode.edges
+    console.log(igposts);
     let feedData = postPreviews.map((x:Post) => {return {
     source: 0,
     date: new Date(x.node.frontmatter.date),
@@ -112,10 +81,9 @@ allInstaNode {
     return (
         <Layout>
             <SEO title="Posts"/>
-            <div className="flex flex-wrap max-w-4xl m-auto">
+            <div className="flex flex-wrap max-w-6xl m-auto">
                 {feedData.map((value: PostContainer, index: number) => {
 
-                    console.log(value)
                     if(value.source === 0 && value.post ){
                     return <PostPreview post={value.post} key={index}></PostPreview>
                         
